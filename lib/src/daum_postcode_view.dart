12475e5e0e9f41e706582ee2a850935bfcb9f52e
onehunnitconst <skygana@gmail.com>
2023-08-31 13:16:46 +0900

    로딩 인디케이터 추가

--- lib/src/daum_postcode_view.dart
+++ lib/src/daum_postcode_view.dart
@@ -2,10 +2,10 @@ import 'dart:convert';
 
 import 'package:daum_postcode/src/model/daum_postcode_model.dart';
 import 'package:daum_postcode/src/options/daum_postcode_options.dart';
-import 'package:flutter/widgets.dart';
+import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
-class DaumPostcodeView extends StatelessWidget {
+class DaumPostcodeView extends StatefulWidget {
   final void Function(DaumPostcodeModel) onComplete;
   final DaumPostcodeOptions? options;
 
@@ -16,30 +16,61 @@ class DaumPostcodeView extends StatelessWidget {
   }) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    final WebViewController postcodePageWebViewController = WebViewController()
+  State<DaumPostcodeView> createState() => _DaumPostcodeViewState();
+}
+
+class _DaumPostcodeViewState extends State<DaumPostcodeView> {
+  bool _isLoading = true;
+  late final WebViewController _postcodePageWebViewController;
+
+  @override
+  void initState() {
+    super.initState();
+    _postcodePageWebViewController = WebViewController()
       ..setJavaScriptMode(JavaScriptMode.unrestricted)
       ..addJavaScriptChannel(
         "onComplete",
         onMessageReceived: _onMessageReceived,
       )
+      ..setNavigationDelegate(
+        NavigationDelegate(
+          onPageFinished: _onPageFinished,
+        ),
+      )
       ..loadRequest(_postcodePageUri());
+  }
 
+  @override
+  Widget build(BuildContext context) {
     return SafeArea(
-      child: WebViewWidget(
-        controller: postcodePageWebViewController,
+      child: Stack(
+        // fit: StackFit.expand,
+        children: [
+          WebViewWidget(
+            controller: _postcodePageWebViewController,
+          ),
+          if (_isLoading) _loadingWidget(),
+        ],
       ),
     );
   }
 
+  Widget _loadingWidget() => const Center(child: CircularProgressIndicator());
+
+  void _onPageFinished(String message) {
+    setState(() {
+      _isLoading = false;
+    });
+  }
+
   void _onMessageReceived(JavaScriptMessage message) {
     final model = DaumPostcodeModel.fromJson(jsonDecode(message.message));
-    onComplete(model);
+    widget.onComplete(model);
   }
 
   Uri _postcodePageUri() => Uri.https(
         "onehunnitconst.github.io",
         "/daum_postcode/pages/postcode.html",
-        options?.toJson(),
+        widget.options?.toJson(),
       );
 }
